b1f23bee5d331833ef492483005dadcbd41ea29b
yutaro.takahashi <jesuisyutaro@gmail.com>
2019-09-21 11:19:44 +0000

    dependent: :destroy

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,8 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
-  has_many :favorites
-  has_many :liked, through: :favorites, source: :user
+  has_many :favorites, dependent: :destroy
+  has_many :liked, through: :favorites, source: :user ,dependent: :destroy
   
   validates :content, presence: true, length: { maximum: 255 }
 end
